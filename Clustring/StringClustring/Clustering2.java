cc8293ab1cfd02cd0950b476baf9f12dca05fc11
sina <s.malakouti2015@aut.ac.ir>
2017-07-26 11:44:37 +0430

    adding description to StringClustring problem

--- Clustring/StringClustring/Clustering2.java
+++ Clustring/StringClustring/Clustering2.java
@@ -8,9 +8,30 @@ import java.util.Scanner;
 /*
  *  The goal of this problem is to run clustring algorithm on 
  * 
- * 
- * 
- * 
+ * 	In this problem we calculate number of disjoint clusters 
+ *	when the maximum spacing is more than 3 
+ * 	
+ *   
+ *
+ *  The file format is:
+ *
+ * [# of nodes] [# of bits for each node's label]
+ *
+ * [first bit of node 1] ... [last bit of node 1]
+ *
+ * [first bit of node 2] ... [last bit of node 2]
+ *
+ * ...
+ *
+ * For example, the third line of the file "0 1 1 0 0 1 1 0 0 1 0 1 1 1 1 1 1 0 1 0 1 1 0 1" denotes the 24 bits associated with node #2.
+ *
+ * The distance between two nodes u and v in this problem is defined as the Hamming distance--- the number of differing bits --- between the two nodes' labels. For example, the Hamming distance between the 24-bit label of node #2 above and the label "0 1 0 0 0 1 0 0 0 1 0 1 1 1 1 1 1 0 1 0 0 1 0 1" is 3 (since they differ in the 3rd, 7th, and 21st bits).
+ *
+ * The question is: 
+ * what is the largest value of k such that there is a k-clustering with spacing at least 3? That is, how many clusters are needed to ensure that no pair of nodes with all but 2 bits in common get split into different clusters?
+ *
+ *
+ *  	answer : 116
  * 
  * 
  */
